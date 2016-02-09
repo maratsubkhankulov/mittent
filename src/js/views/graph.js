c875389b7d2d930c67662159087ff726fa733e99
Marat Subkhankulov <skifmg@hotmail.com>
2016-02-09 11:28:25 +0000

    Greyed out unfinished hours; Coloured current

--- src/js/views/graph.js
+++ src/js/views/graph.js
@@ -116,11 +116,11 @@ directory.GraphView = Backbone.View.extend({
             console.log("No log entries to graph");
             return;
         }
-        var today = moment();
+        var now = moment();
         var min = moment(entries[entries.length - 1].get('datetime'));
         var minDay = moment(min).startOf('day');
         var max = moment(entries[0].get('datetime'));
-        var numDays = today.diff(min, 'd') + 1;
+        var numDays = now.diff(min, 'd') + 1;
 
         // Colour eating slots
         var myself = this;
@@ -129,14 +129,21 @@ directory.GraphView = Backbone.View.extend({
             var currentDay = moment(current).startOf('day');
             var barIdx = currentDay.diff(minDay,'d');
             var segmentIdx = current.hour();
-            myself.colourSlot(barIdx, segmentIdx, entry.get('comment'));
+            myself.colourSlot(barIdx, segmentIdx, entry.get('comment'), "warning");
         });
+
+        // Update current hour
+        var hour = now.hours();
+        this.colourSlot(numDays, hour, "", "info");
+        for (hour++; hour < 24; hour++) {
+            this.colourSlot(numDays, hour, "", "info hidden");
+        }
     },
 
-    colourSlot: function(barIdx, segmentIdx, title) {
+    colourSlot: function(barIdx, segmentIdx, title, style) {
         var segmentView = this.barViews[barIdx].segmentViews[segmentIdx];
         segmentView.model.title = title;
-        segmentView.model.style = "warning";
+        segmentView.model.style = style;
         segmentView.render();
         this.barViews[barIdx].render();
     },
