feeee52f1b81421622c151b45b2773db658ee88d
Marat Subkhankulov <skifmg@hotmail.com>
2016-02-09 18:45:22 +0000

    Add subtle background

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 # Building right now just means copying the source files
-mkdir ./build
+mkdir --path ./build
+echo "copying src files into build"
 cp -r ./src/* ./build
-echo "done"
+
+echo "copying build files into maratsubkhankulov.github.io"
+cp -r ./build/* ./maratsubkhankulov.github.io
+
+echo "adding all to repo and committing"
+cd maratsubkhankulov.github.io
+git add *
+
+echo "pushing to remote"
+git push remote master
