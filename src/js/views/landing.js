de5b07f225c561713ad934e30a441f88fec4487d
Marat Subkhankulov <skifmg@hotmail.com>
2016-02-11 22:37:22 +0000

    added a few missing files

---
+++ src/js/views/landing.js
@@ -0,0 +1,9 @@
+directory.LandingView = Backbone.View.extend({
+    initialize: function() {
+    },
+    
+    render:function () {
+        this.$el.html(this.template());
+        return this;
+    }
+});
