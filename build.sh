4a36e76a489bf5340b05b0fced425bfaabf047c2
Marat Subkhankulov <skifmg@hotmail.com>
2016-03-05 11:19:33 +0000

    updated build script to not use personal pages

--- build.sh
+++ build.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 # Building right now just means copying the source files
-mkdir --path ./build
+mkdir ./build
 echo "copying src files into build"
 cp -r ./src/* ./build
-
-echo "copying build files into maratsubkhankulov.github.io"
-cp -r ./build/* ./maratsubkhankulov.github.io
-
-echo "adding all to repo and committing"
-cd maratsubkhankulov.github.io
-git add *
-git commit -am "io update"
